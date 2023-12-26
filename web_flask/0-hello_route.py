a6bb0247c0c6f1057a79668d86547f9001aad70c
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-12-26 14:04:37 +0100

    Start Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module Name: 0-hello_route.py
+Description: This module defines the start of Flask application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbtn():
+    """ Display “Hello HBNB!” """
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
